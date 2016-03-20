ff0c0e5543fa914f53b9768e3a4b1a89171359ad
Deesen <dh@fuseit.de>
2016-03-20 17:18:28 +0100

    Enable iFrame-scrolling

--- assets/modules/mytinytodo/module.mytinytodo.php
+++ assets/modules/mytinytodo/module.mytinytodo.php
@@ -2,4 +2,4 @@
 // Updates / Issues: https://github.com/Deesen/mytinytodo-for-modx-evo
 
 // Easy solution instead of using Modx-session
-echo '<iframe src="/assets/modules/mytinytodo/lib/index.php" frameborder="0" style="overflow:hidden;height:100%;width:100%" height="100%" width="100%" scrolling="yes" seamless="seamless"></iframe>';
+echo '<iframe src="/assets/modules/mytinytodo/lib/index.php" frameborder="0" style="overflow-y:auto;height:100%;width:100%" height="100%" width="100%" scrolling="auto" seamless="seamless"></iframe>';
